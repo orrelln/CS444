a4c3443ba51719de8c3b2f93750bb5a2c6b9af2b
willmichael <oleemir@gmail.com>
2017-04-17 16:16:00 -0700

    compiles

--- project1/concurrency/mconcurrency.c
+++ project1/concurrency/mconcurrency.c
@@ -8,30 +8,39 @@
 void *ProducerThread(void *item);
 bool isBufferFull();
 
+// Vars
+pthread_mutex_t mutex;
+
+// Item in buffer should be a struct with 2 numbers
+typedef struct { 
+        int consumerNum;
+        int randWait;
+} BufferItem;
+
 // Global Buffer
 BufferItem bufferArr[32];
 
-// Iitem in buffer should be a struct with 2 numbers
-typedef struct { 
-    int consumerNum;
-    int randWait;
-} BufferItem;
+
+int main(int argc, char *argv[]) {
+        //pthread identifier
+        pthread_t producerT;
+
+        pthread_create(&producerT, NULL, ProducerThread, NULL);
 
 
-int main(int argc, char *argv) {
 
 }
 
 
 
-/
+/*
  * Param: item to put into buffer
  * Adds item to the buffer, while adding has exclusing access to buffer
  * if buffer is full, block until producer adds a new item
  * Post: item is added to buffer
  * 
  * http://timmurphy.org/2010/05/04/pthreads-in-c-a-minimal-working-example/
- /
+ */
 void *ProducerThread(void *item) {
     
 
