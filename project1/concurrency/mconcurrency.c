2758676ecd76f12f6939ba27a6bd8f4e3383a8c5
willmichael <oleemir@gmail.com>
2017-04-17 16:04:13 -0700

    buffer commit

--- project1/concurrency/mconcurrency.c
+++ project1/concurrency/mconcurrency.c
@@ -2,14 +2,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <stdbool.h>
 
-//item in buffer should be a struct with 2 numbers
-struct BufferItem { 
+// Headers
+void *ProducerThread(void *item);
+bool isBufferFull();
+
+// Global Buffer
+BufferItem bufferArr[32];
+
+// Iitem in buffer should be a struct with 2 numbers
+typedef struct { 
     int consumerNum;
     int randWait;
-}
+} BufferItem;
+
 
 int main(int argc, char *argv) {
 
 }
+
+
+
+/
+ * Param: item to put into buffer
+ * Adds item to the buffer, while adding has exclusing access to buffer
+ * if buffer is full, block until producer adds a new item
+ * Post: item is added to buffer
+ * 
+ * http://timmurphy.org/2010/05/04/pthreads-in-c-a-minimal-working-example/
+ /
+void *ProducerThread(void *item) {
+    
+
+
+
+    return 0;
+}
+
+
 
