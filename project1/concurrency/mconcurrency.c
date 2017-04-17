185b222b9af1f901c1b369821ffffffc77f4a4cc
Aileen <dapper.whale@gmail.com>
2017-04-17 16:54:30 -0700

    Start work on consumer

--- project1/concurrency/mconcurrency.c
+++ project1/concurrency/mconcurrency.c
@@ -2,21 +2,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <time.h>
 #include <stdbool.h>
 
 // Headers
 void *ProducerThread(void *item);
+void *consumer();
 bool isBufferFull();
 
 // Global Buffer
 BufferItem bufferArr[32];
 
-// Iitem in buffer should be a struct with 2 numbers
+// Item in buffer should be a struct with 2 numbers
 typedef struct { 
     int consumerNum;
     int randWait;
 } BufferItem;
 
+pthread_cond_t BufferNotFull=PTHREAD_COND_INITIALIZER;
+pthread_cond_t BufferFull=PTHREAD_COND_INITIALIZER;
+pthread_cond_t BufferNotEmpty=PTHREAD_COND_INITIALIZER;
+pthread_cond_t BufferEmpty=PTHREAD_COND_INITIALIZER;
+pthread_mutex_t bufferId=PTHREAD_MUTEX_INITIALIZER;
 
 int main(int argc, char *argv) {
 
@@ -24,14 +31,14 @@ int main(int argc, char *argv) {
 
 
 
-/
+/*
  * Param: item to put into buffer
  * Adds item to the buffer, while adding has exclusing access to buffer
  * if buffer is full, block until producer adds a new item
  * Post: item is added to buffer
  * 
  * http://timmurphy.org/2010/05/04/pthreads-in-c-a-minimal-working-example/
- /
+ */
 void *ProducerThread(void *item) {
     
 
@@ -41,4 +48,35 @@ void *ProducerThread(void *item) {
 }
 
 
+/* Requirements/constraints
+ * -- While an item is being added to or removed from the buffer, the buffer is
+      in an inconsistent state. Therefore, threads must have exclusive access
+      to the buffer
+ * -- If a consumer thread arrives while the buffer is empty, it blocks until a
+      producer adds a new item. 
+ * -- If a producer thread has an item to put in the buffer while the buffer is
+      full, it blocks until a consumer removes an item.
+ */
+void *consumer() {
+    //struct BufferItem consumeN;
+    //struct BufferItem waitTime;
+    //fprintf("%d", BufferItem->consumerNum);
+
+    if (&bufferNotFull){
+        // Lock thread while removing item from buffer
+        pthread_mutex_lock(&bufferId);
+        // do thing
+        pthread_mutex_unlock(&bufferId);
+    }else if (&bufferEmpty){
+        // if buffer empty, wait until a "not empty" signal is sent
+        pthread_cond_wait(&bufferNotEmpty, &bufferId);
+    }else if (&bufferFull){
+        // if the buffer is full, lock and have consumer thread remove item
+        pthread_mutex_lock(&bufferId);
+        // Print first val
+        fprintf("Consumed: %d\n", consumeN); //<-- ??
+        pthread_mutex_unlock(&bufferId);
+        pthread_cond_signal(&bufferNotFull);
+    }
+}
 
