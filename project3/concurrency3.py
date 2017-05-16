94557ff46b25b4438ec03d6bb24068951ad57cc7
willmichael <oleemir@gmail.com>
2017-05-16 15:46:06 -0700

    init

---
+++ project3/concurrency3.py
@@ -0,0 +1,26 @@
+#http://interactivepython.org/courselib/static/pythonds/BasicDS/ImplementinganUnorderedListLinkedLists.html
+
+class Node:
+    def __init__(self,initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self,newdata):
+        self.data = newdata
+
+    def setNext(self,newnext):
+        self.next = newnext
+
+
+def main():
+    pass
+
+
+if __name__ == __main__:
+    main()
