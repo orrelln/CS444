bc76d9e7f8663f837c0b67068510e31bbf099199
willmichael <oleemir@gmail.com>
2017-05-18 16:23:42 -0700

    fixed errors with misnamed inserting lock, added testing cases

--- project3/concurrency3.py
+++ project3/concurrency3.py
@@ -75,7 +75,7 @@ class LinkedList:
         current = self.head
         sys.stdout.write("PRINTING LIST STATE: ")
         while current != None:
-            sys.stdout.write(str(self.head.getData()) + ", ")
+            sys.stdout.write(str(current.getData()) + ", ")
             sys.stdout.flush()
             current = current.getNext()
         sys.stdout.write("\n")
@@ -83,17 +83,15 @@ class LinkedList:
         
 
 
-# define global linked list
-# globList = LinkedList()
-
-notSearch = Lock()
-notInsert = Lock()
-searchTempLock = Lock()
-insertTempLock = Lock()
-insertCompLock = Lock()
-
-
 manager = Manager()
+
+notSearch = manager.Lock()
+notInsert = manager.Lock()
+searchTempLock = manager.Lock()
+insertTempLock = manager.Lock()
+insertCompLock = manager.Lock()
+
+
 numSearchers = manager.Value('i',0)
 numInserters = manager.Value('i',0)
 
@@ -109,117 +107,152 @@ linkedList = man.link()
 def main():
     random.seed()
 
-    # global linkedList
-    # linkedList.add(0)
-    # linkedList.add(1)
-    # linkedList.add(2)
-    # linkedList.add(3)
-
-    # linkedList.remove(3)
-    # if linkedList.search(4):
-        # linkedList.remove(4)
-
+    ### Test inserting at same time
+    print "----Test inserting at same time"
 
     i = Process(target=inserter)
+    i2 = Process(target=inserter)
     i.start()
+    i2.start()
+    
+    i.join()
+    i2.join()
 
+    ### Test searching at same time
+
+    print "----Test searching at same time"
     s = Process(target=searcher)
-    s.start()
-
     s1 = Process(target=searcher)
+    s.start()
     s1.start()
 
-    data = random.randint(0,10)
+    s.join()
+    s1.join()
+    
+    ### Test deleting at same time
+
+    print "----Test deleting at same time"
     d = Process(target=deleter)
+    d2 = Process(target=deleter)
+    d.start()
+    d2.start()
+
+    d.join()
+    d2.join()
+
+    ### Test inserting and searching at same time
+
+    print "----Test inserting and searching at same time"
+    i = Process(target=inserter)
+    s = Process(target=searcher)
+    i.start()
+    s.start()
+
+    i.join()
+    s.join()
+
+    ### Test inserting and searching and deleting at the same time
+
+    print "----Test inserting and searching and deleting at the same time"
+    i = Process(target=inserter)
+    s = Process(target=searcher)
+    d = Process(target=deleter)
+    i.start()
+    s.start()
     d.start()
 
     i.join()
     s.join()
-    s1.join()
     d.join()
 
 def inserter():
-    while(1):
-        global linkedList
-        global numInserters
-        insertTempLock.acquire()
+    # while(1):
+    global linkedList
+    global numInserters
+    global insertTempLock
+    global insertCompLock
+    insertTempLock.acquire()
 
-        numInserters.value += 1
-        idInsert = numInserters.value
-        if numInserters.value == 1:
-            notSearch.acquire()
+    numInserters.value += 1
+    idInsert = numInserters.value
+    if numInserters.value == 1:
+        notInsert.acquire()
 
-        insertTempLock.release()
-        insertCompLock.acquire()
+    insertTempLock.release()
+    insertCompLock.acquire()
+    print "INSERTING"
+    # insert here
+    print "inserting " + str(idInsert)
+    linkedList.add(idInsert)
+    linkedList.printList()
+    time.sleep(2.0)
+    print "end inserting"
+    print
 
-        # insert here
-        print "inserting " + str(idInsert)
-        linkedList.add(idInsert)
-        linkedList.printList()
-        time.sleep(5.0)
-        print "end inserting"
-        print
+    insertCompLock.release()
+    insertTempLock.acquire()
 
-        insertCompLock.release()
-        insertTempLock.acquire()
+    numInserters.value -= 1
+    if numInserters.value == 0:
+        notInsert.release()
 
-        numInserters.value -= 1
-        if numInserters.value == 0:
-            notSearch.release()
-
-        insertTempLock.release()
+    insertTempLock.release()
 
 def searcher():
-    while(1):
-        global linkedList
-        global numSearchers
-        searchTempLock.acquire()
-
-        numSearchers.value += 1
-        idSearcher = numSearchers.value
-        if numSearchers.value == 1:
-            notSearch.acquire()
-
-        searchTempLock.release()
-
-        # search here
-        print "searching for " + str(idSearcher)
-        linkedList.printList()
-        print "Found " + str(linkedList.search(idSearcher))
-        time.sleep(2.0)
-        print "end searching"
-        print
-
-        searchTempLock.acquire()
-        numSearchers.value -= 1
-        if numSearchers.value == 0:
-            notSearch.release()
-        searchTempLock.release()
-
-def deleter():
-    while(1):
-        global linkedList
-        notInsert.acquire()
+    # while(1):
+    global linkedList
+    global numSearchers
+    global searchTempLock
+    global notSearch
+    searchTempLock.acquire()
+    numSearchers.value += 1
+    idSearcher = numSearchers.value
+    if numSearchers.value == 1:
         notSearch.acquire()
 
-        # delete
-        data = random.randint(0,10)
-        print "attempting to delete " + str(data)
-        linkedList.printList()
-        if linkedList.search(data):
-            linkedList.remove(data)
-            print "successfully deleted"
-            linkedList.printList()
-        else:
-            print "item not found"
-        time.sleep(2.0)
-        print "end deleting"
-        print
+    print "SEARCHING"
+    searchTempLock.release()
 
-        notInsert.release()
+    # search here
+    print "searching for " + str(idSearcher)
+    linkedList.printList()
+    print "Found " + str(linkedList.search(idSearcher))
+    time.sleep(2.0)
+    print "end searching"
+    print
+    searchTempLock.acquire()
+    numSearchers.value -= 1
+    if numSearchers.value == 0:
         notSearch.release()
+    searchTempLock.release()
+
+def deleter():
+    # while(1):
+    global linkedList
+    global notSearch
+    global notInsert
+    notInsert.acquire()
+    notSearch.acquire()
+    print "DELETING"
+
+    # delete
+    data = random.randint(0,10)
+    print "attempting to delete " + str(data)
+    linkedList.printList()
+    if linkedList.search(data):
+        linkedList.remove(data)
+        print "successfully deleted"
+        linkedList.printList()
+    else:
+        print "item not found"
+    time.sleep(2.0)
+    print "end deleting"
+    print
+
+    notInsert.release()
+    notSearch.release()
 
 
 
-if __name__ == "__main__":
+if __name__=='__main__':
     main()
