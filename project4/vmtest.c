2d027e4946d436a2cbd62838d4c892fb96ea5a37
willmichael <oleemir@gmail.com>
2017-06-05 00:41:35 -0700

    Added vm test

---
+++ project4/vmtest.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+        // Define our system call numbers
+        int sys_call_free = 601;
+        int sys_call_used = 600;
+
+        // Create dummy arrays to artificially make process large
+        int * dummy_int0, dummy_int1, dummy_int2, dummy_int3, dummy_int4; 
+        dummy_int0 = (int*)malloc(100000);
+        dummy_int1 = (int*)malloc(50000);
+        dummy_int2 = (int*)malloc(60000);
+        dummy_int3 = (int*)malloc(910000);
+        dummy_int4 = (int*)malloc(13000);
+
+        // Print our statistics
+        printf("Free Bytes: %d\n", syscall(sys_call_free));
+        printf("Used Bytes: %d\n", syscall(sys_call_used));
+        return 0;
+}
+
